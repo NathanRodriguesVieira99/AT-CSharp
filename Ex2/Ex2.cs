1b7861819f5820e40fd896b8a7760dda6f684e9f
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-31 20:20:04 -0300

    feat: ex 2 done

---
+++ Ex2/Ex2.cs
@@ -0,0 +1,37 @@
+﻿class Ex2
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe seu nome: ");
+        string name = Console.ReadLine();
+
+        string nameLetrasDeslocadas = deslocaLetra(name, 3);
+
+        Console.WriteLine($"Seu nome com as letras deslocadas: {nameLetrasDeslocadas}");
+    }
+
+    static string deslocaLetra(String letra, int desloca)
+    {
+        String nameLetrasDeslocadas = "";
+
+        foreach (char a in letra)
+
+        {
+            if (char.IsLetter(a))
+            {
+                char letraInicial = char.IsUpper(a) ? 'A' : 'a';
+
+
+                char letraDeslocada = (char)(letraInicial + ((a + desloca - letraInicial) % 26));
+                nameLetrasDeslocadas += letraDeslocada;
+            }
+
+            else
+            {
+                nameLetrasDeslocadas += a;
+            }
+
+        }
+        return nameLetrasDeslocadas;
+    }
+}
