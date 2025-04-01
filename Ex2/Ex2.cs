3c665332026e8dca0f5bb363b8e6a54bee4afc54
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-01 08:51:18 -0300

    feat: add comments to explain better

--- Ex2/Ex2.cs
+++ Ex2/Ex2.cs
@@ -5,20 +5,24 @@
         Console.WriteLine("Informe seu nome: ");
         string name = Console.ReadLine();
 
+        // chama a função de deslocar a letra em 3 posições
         string nameLetrasDeslocadas = deslocaLetra(name, 3);
 
         Console.WriteLine($"Seu nome com as letras deslocadas: {nameLetrasDeslocadas}");
     }
 
+    // essa é a função que desloca as letras
     static string deslocaLetra(String letra, int desloca)
     {
         String nameLetrasDeslocadas = "";
 
+        // percorre cada letra
         foreach (char a in letra)
 
         {
             if (char.IsLetter(a))
             {
+                // verifica se a inicial é maiuscula
                 char letraInicial = char.IsUpper(a) ? 'A' : 'a';
 
 
